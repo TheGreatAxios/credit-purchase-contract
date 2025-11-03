42299846ca6f994d1ee1a707e9fd3035eb1f0e03
TheGreatAxios <sawyer@dirtroad.dev>
2025-11-03 13:14:12 -0800

    Format

--- src/CreditPurchase.sol
+++ src/CreditPurchase.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
-import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
-import { Pausable } from "@openzeppelin/contracts/utils/Pausable.sol";
-import { ReentrancyGuard } from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
-import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {Pausable} from "@openzeppelin/contracts/utils/Pausable.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 error InsufficientFunds();
 error InvalidCreditToken();
@@ -46,5 +46,4 @@ contract CreditPurchase is Ownable, Pausable, ReentrancyGuard {
         emit CreditsBought(creditToken, NUMBER_OF_CREDITS, price);
     }
 }
-    
 
